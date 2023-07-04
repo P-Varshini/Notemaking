b16cd1dd36656c1c22636241bd5df01f8d4da5fd
P-Varshini <119688191+P-Varshini@users.noreply.github.com>
2023-07-04 11:48:35 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+export default App;
